882bdba1a81507a1c2f99f80481eab241b5a2d56
ZauAlpha <zaurielec@gmail.com>
2023-04-17 10:51:31 -0600

    Agregar archivos de proyecto.

---
+++ LIVE_DEMO/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LIVE_DEMO
+{
+    public partial class MAIN_FORM : Form
+    {
+        Rasterization raster;
+        public MAIN_FORM()
+        {
+            InitializeComponent();
+        }
+                    
+        private void BTN_EXE_Click(object sender, EventArgs e)
+        {
+            raster.Scales(0.5f);
+        }
+
+        private void Init()
+        {
+            
+
+        }
+
+
+        private void PCT_CANVAS_SizeChanged(object sender, EventArgs e)
+        {
+            if(raster != null)
+                raster.Resize(PCT_CANVAS.Size);
+        }
+
+        private void TIMER_Tick(object sender, EventArgs e)
+        {
+            if (raster == null)
+                return;
+            raster.Render();
+            //raster.Rotate(0.1f);
+            //raster.Translate(new Vertex(0.1f,0, 0.1f));
+            PCT_CANVAS.Invalidate();
+        }
+
+        private void MAIN_FORM_Load(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+      
+
+        private void Translate_Click(object sender, EventArgs e)
+        {
+            if (raster == null)
+                return;
+            if (translationY.Text.Length == 0)
+                translationY.Text = "0";
+            if (translationX.Text.Length == 0)
+                translationX.Text = "0";
+            if (translationZ.Text.Length == 0)
+                translationZ.Text = "0";
+            float x = float.Parse(translationX.Text);
+            float y = float.Parse(translationY.Text);
+            float z = float.Parse(translationZ.Text);
+            raster.Translate(new Vertex(x, y, z));
+        }
+
+        private void Scale_Click(object sender, EventArgs e)
+        {
+            if (raster == null)
+                return;
+            float scale = float.Parse(scalation.Text);
+            raster.Scales(scale);
+        }
+        private String formatTranslation(String text)
+        {
+            return new string(text.Where(c => char.IsDigit(c) || c == '.' || c == '-').ToArray());
+        }
+
+        private void translationX_TextChanged(object sender, EventArgs e)
+        {
+            translationX.Text=formatTranslation(translationX.Text);
+        }
+
+        private void translationY_TextChanged(object sender, EventArgs e)
+        {
+            translationY.Text=formatTranslation(translationY.Text);
+        }
+
+        private void translationZ_TextChanged(object sender, EventArgs e)
+        {
+            translationZ.Text=formatTranslation(translationZ.Text);
+        }
+
+        private void Rotate_Click(object sender, EventArgs e)
+        {
+            if (raster == null)
+                return;
+            float x = (float)rotationXUpDown.Value;
+            float y = (float)rotationYUpDown.Value;
+            float z = (float)rotationZUpDown.Value;
+
+            raster.Rotate(new Vertex(x, y, z));
+            
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            { 
+                string filePath = openFileDialog1.FileName;
+                List<Vertex> vertexes = new List<Vertex>();
+                List<Triangle> triangles = new List<Triangle>();
+                StreamReader streamReader = new StreamReader(filePath);
+                //String[] lines = streamReader.ReadToEnd().Split('\n');
+                using (streamReader) {
+                    string line;
+                    while((line = streamReader.ReadLine()) != null)
+                    {
+                        List<int> indexes = new List<int>();
+                        if (line.StartsWith("v "))
+                        {
+                            string[] tokens = line.Split(' ');
+                            float x = float.Parse(tokens[1]);
+                            float y = float.Parse(tokens[2]);
+                            float z = float.Parse(tokens[3]);
+                            Vertex ver = new Vertex(x, y, z);
+                            vertexes.Add(ver);
+                            Console.WriteLine("Vertex " + ver);
+                        }
+                        else if (line.StartsWith("f "))
+                        {
+                            
+                            string[] aux = line.Split(' ');
+                            for(int i=1; i<aux.Length; i++)
+                            {
+                                indexes.Add(int.Parse(aux[i].Split('/')[0])-1);
+                            }
+                            Triangle triangle = new Triangle(indexes[0], indexes[1], indexes[2], Color.White);
+                            triangles.Add(triangle);
+                            Console.WriteLine(triangle);
+                            //PrintArray(aux);
+                            
+                        }
+                    }
+                }
+                if(raster == null)
+                {
+                    raster = new Rasterization(PCT_CANVAS.Size, vertexes, triangles);
+                    PCT_CANVAS.Image = raster.Canvas;
+                }
+                else
+                {
+                    raster.AddModel(vertexes, triangles);
+                }
+                
+
+
+                
+                
+            }
+        }
+        public static void PrintArray(string[] array)
+        {
+            Console.WriteLine();
+            Console.Write("[ ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.Write(" ]");
+        }
+
+        
+
+    }
+}
---
+++ LIVE_DEMO/Rasterization.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Diagnostics;
+
+namespace LIVE_DEMO
+{
+    public class Rasterization
+    {
+        // Scene setup.
+        float viewport_size         = 1;
+        float projection_plane_z    = 1;
+        Bitmap canvas;
+        Graphics canvas_context;
+        int canvas_width;
+        int canvas_height;
+        List<Model> models;
+        Model cube;
+        Camera camera   = new Camera(new Vertex(0,0, 2), Mtx.RotY(0));
+       // float s2        = (float)Math.Sqrt(2);
+
+        List<Instance> instances;
+
+        public Bitmap Canvas
+        {
+            get { return canvas; }
+        }
+
+        Vertex[] vertices = new Vertex[] {
+                                            new Vertex(1, 1, 1),
+                                            new Vertex(-1, 1, 1),
+                                            new Vertex(-1, -1, 1),
+                                            new Vertex(1, -1, 1),
+                                            new Vertex(1, 1, -1),
+                                            new Vertex(-1, 1, -1),
+                                            new Vertex(-1, -1, -1),
+                                            new Vertex(1, -1, -1)
+                                        };
+
+
+        Triangle[] triangulos = new Triangle[] {             
+                                            new Triangle(0, 1, 2, Color.Red),
+                                            new Triangle(0, 2, 3, Color.Red),
+                                            new Triangle(4, 0, 3, Color.Green),
+                                            new Triangle(4, 3, 7, Color.Green),
+                                            new Triangle(5, 4, 7, Color.Blue),
+                                            new Triangle(5, 7, 6, Color.Blue),
+                                            new Triangle(1, 5, 6, Color.Yellow),
+                                            new Triangle(1, 6, 2, Color.Yellow),
+                                            new Triangle(4, 5, 1, Color.Purple),
+                                            new Triangle(4, 1, 0, Color.Purple),
+                                            new Triangle(2, 6, 7, Color.Cyan),
+                                            new Triangle(2, 7, 3, Color.Cyan)
+                                           };
+
+        public Rasterization(Size size)
+        {
+            Init(size);
+        }
+        public Rasterization(Size size, List<Vertex> vertexes, List<Triangle> triangles) { 
+            this.vertices = vertexes.ToArray();
+            this.triangulos = triangles.ToArray();
+            Init(size);
+        }
+        public void White()
+        {
+            canvas_context.Clear(Color.White);
+        }
+        public void Resize(Size size)
+        {
+            canvas = new Bitmap(size.Width, size.Height);
+            canvas_context = Graphics.FromImage(canvas);
+            canvas_width = canvas.Width;
+            canvas_height = canvas.Height;
+        }
+        private void Init(Size size)
+        {
+            canvas = new Bitmap(size.Width, size.Height);
+            canvas_context = Graphics.FromImage(canvas);
+            canvas_width = canvas.Width;
+            canvas_height = canvas.Height;
+            models = new List<Model>();
+            models.Add(new Model(vertices, triangulos, new Vertex(0, 0, 0), (float)Math.Sqrt(3)));
+            
+            instances = new List<Instance> {
+                // new Instance(cube, new Vertex( -1.25f,    0,    7 ), Mtx.Identity, 0.75f),
+                // new Instance(cube, new Vertex(  0f, 0f, 7.5f ), Mtx.RotZ(45),0.5f),
+                 new Instance(models[0], new Vertex(     0,     0, 7.5f ), Mtx.RotY(0))};
+        
+            // here is not implemented the FOV in the view only the planes at that angle
+            // to implement the FOV in the canvas we need to create also that matrix
+            // of the camera with that same angle
+            ComputePlanes(120);//degrees
+
+            
+        }
+        public void AddModel(List<Vertex> vertexes, List<Triangle> triangles)
+        {
+            Model model = new Model(vertexes.ToArray(), triangles.ToArray(), new Vertex(0, 0, 0), (float)Math.Sqrt(3));
+            models.Add(model);
+            instances.Add(new Instance(model, new Vertex(0, 0, 7.5f), Mtx.RotY(0)));
+        }
+       
+
+        
+
+        public void ComputePlanes(float fov)
+        {
+            Vertex left, right, bottom, top;
+            float aspect = 1f;
+            float near = 0.1f;
+
+            float tanFov = (float)Math.Tan(fov * 0.5f * Math.PI / 180f);
+            float height = 2f * tanFov * near;
+            float width = height * aspect;
+
+            // Left plane
+            float sx = 1f * (width / 2f);
+            float sy = 0f;
+            float sz = near;
+
+            left = new Vertex(sx, sy, sz);
+            left = left.Normalize();
+
+            // Right plane
+            sx = -width / 2f;
+            sy = 0f;
+            sz = near;
+            right = new Vertex(sx, sy, sz);
+            right = right.Normalize();
+
+            // Bottom plane
+            sx = 0f;
+            sy = -1f * (height / 2f);
+            sz = near;
+            bottom = new Vertex(sx, sy, sz);
+            bottom = bottom.Normalize();
+
+            // Top plane
+            sx = 0f;
+            sy = height / 2f;
+            sz = near;
+            top = new Vertex(sx, sy, sz);
+            top = top.Normalize();
+            
+            camera.clipping_planes.Add(new Plane(new Vertex(0, 0, 1), 0));   // near
+            camera.clipping_planes.Add(new Plane(left,0));  // left
+            camera.clipping_planes.Add(new Plane(right, 0));  // right
+            camera.clipping_planes.Add(new Plane(top, 0));  // top
+            camera.clipping_planes.Add(new Plane(bottom, 0));  // bottom
+        }
+
+        public void Render()
+        {
+            FastClear();
+            RenderScene(camera, instances.ToArray());
+            
+        }
+
+        // The PutPixel() function.
+        private void PutPixel(int x, int y, Color color)
+        {
+            x = canvas_width / 2 + x;
+            y = canvas_height / 2 - y - 1;
+
+            if (x < 0 || x >= canvas_width || y < 0 || y >= canvas_height)
+            {
+                return;
+            }
+
+            canvas.SetPixel(x, y, color);
+        }
+
+        List<float> Interpolate(int i0, float d0, int i1, float d1)
+        {
+            if (i0 == i1)
+            {
+                return new List<float> { d0 };
+            }
+
+            List<float> values = new List<float>();
+            float a = (d1 - d0) / (i1 - i0);
+            float d = d0;
+            for (var i = i0; i <= i1; i++)
+            {
+                values.Add(d);
+                d += a;
+            }
+
+            return values;
+        }
+
+        private void Swap(ref Vertex a, ref Vertex b)
+        {
+            Vertex temp = a;
+            a = b;
+            b = temp;
+        }
+
+        void DrawLine(Vertex p0, Vertex p1, Color color)
+        {
+            var dx = p1.x - p0.x;
+            var dy = p1.y - p0.y;
+
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+                // The line is horizontal-ish. Make sure it's left to right.
+                if (dx < 0)
+                {                    
+                    Swap(ref p0, ref p1);
+                }
+
+                // Compute the Y values and draw.
+                var ys = Interpolate((int)p0.x, p0.y, (int)p1.x, p1.y);
+                for (var x = (int)p0.x; x <= p1.x; x++)
+                {
+                    PutPixel(x, (int)ys[(x - (int)p0.x)], color);
+                }
+            }
+            else
+            {
+                // The line is vertical-ish. Make sure it's bottom to top.
+                if (dy < 0)
+                {
+                    Swap(ref p0, ref p1);
+                }
+
+                // Compute the X values and draw.
+                var xs = Interpolate((int)p0.y, p0.x, (int)p1.y, p1.x);
+                for (var y = (int)p0.y; y <= p1.y; y++)
+                {
+                    PutPixel((int)xs[(y - (int)p0.y)], y, color);
+                }
+            }
+        }
+
+
+        void DrawWireframeTriangle(Vertex p0, Vertex p1, Vertex p2, Color color)
+        {
+            DrawLine(p0, p1, color);
+            DrawLine(p1, p2, color);
+            DrawLine(p0, p2, color);
+        }
+
+        // Converts 2D viewport coordinates to 2D canvas coordinates.
+        Vertex ViewportToCanvas(Vertex p2d)
+        {
+            float vW = (float)canvas.Width / canvas.Height;
+            return new Vertex((p2d.x * canvas.Width / vW), (p2d.y * canvas.Height / viewport_size), 0);
+        }
+
+        Vertex ProjectVertex(Vertex v)
+        {
+            return ViewportToCanvas(new Vertex(v.x * projection_plane_z / v.z, v.y * projection_plane_z / v.z, 0));
+        }
+
+        void RenderTriangle(Triangle triangle, List<Vertex> projected)
+        {
+            DrawWireframeTriangle(projected[triangle.v0], projected[triangle.v1], projected[triangle.v2], triangle.color);
+            
+
+        }
+
+        // Clips a triangle against a plane. Adds output to triangles and vertices.
+        private List<Triangle> ClipTriangle(Triangle triangle, Plane plane, List<Triangle> triangles, List<Vertex> vertexes)
+        {
+            
+            Vertex v0 = vertexes[triangle.v0];
+            Vertex v1 = vertexes[triangle.v1];
+            Vertex v2 = vertexes[triangle.v2];
+
+            // vertexes in front of the camera
+            bool in0 = ((plane.normal * v0) + plane.Distance) > 0;
+            bool in1 = ((plane.normal * v1) + plane.Distance) > 0;
+            bool in2 = ((plane.normal * v2) + plane.Distance) > 0;
+
+            int in_count = (in0 ? 1 : 0) + (in1 ? 1 : 0) + (in2 ? 1 : 0);
+
+            if (in_count == 0)
+            {
+                //Console.WriteLine("count zero");
+                // Nothing to do - the triangle is fully clipped out.
+            }
+            else if (in_count == 3)
+            {
+                // The triangle is fully in front of the plane.
+                triangles.Add(triangle);
+            }
+            else if (in_count == 1)// one positive  
+            {
+                //Console.WriteLine("count one");
+                // The triangle has one vertex in. Output is one clipped triangle.
+            }
+            else if (in_count == 2)// one negative
+            {
+                //Console.WriteLine("count two");
+                // The triangle has two vertices in. Output is two clipped triangles.
+            }
+
+            return triangles;
+        }
+
+        private Model TransformAndClip(Plane[] clipping_planes, Model model, float scale, Mtx transform)
+        {
+            // Transform the bounding sphere, and attempt early discard.
+            Vertex center = transform * model.bounds_center;
+            float radius = model.bounds_radius * scale;
+            for (int p = 0; p < clipping_planes.Length; p++)
+            {
+                float distance = (clipping_planes[p].normal * center) + clipping_planes[p].Distance;
+                if (distance < -radius)
+                {
+                    return null;
+                }
+            }
+
+            // Apply modelview transform.
+            List<Vertex> vertices = new List<Vertex>();
+            for (int i = 0; i < model.vertices.Length; i++)
+            {
+                vertices.Add(transform * model.vertices[i]);
+            }
+
+            // Clip the entire model against each successive plane.
+            List<Triangle> triangles = new List<Triangle>(model.triangles);
+            for (int p = 0; p < clipping_planes.Length; p++)
+            {
+                List<Triangle> new_triangles = new List<Triangle>();
+                for (int i = 0; i < triangles.Count; i++)
+                {
+                    new_triangles = (ClipTriangle(triangles[i], clipping_planes[p], new_triangles, vertices));
+                }
+                triangles.AddRange(new_triangles);
+            }
+
+            return new Model(vertices.ToArray(), triangles.ToArray(), center, model.bounds_radius);
+        }
+
+        private void RenderModel(Model model)
+        {
+            // we would have to test here the best fit to
+            // translate this to the GPU for massive parallelism
+            List<Vertex> projected = new List<Vertex>();
+            
+            for (int i = 0; i < model.vertices.Length; i++)
+            {
+                projected.Add(ProjectVertex(model.vertices[i]));
+            }
+
+            for (int i = 0; i < model.triangles.Length; i++)
+            {
+                RenderTriangle(model.triangles[i], projected);
+            }
+        }
+
+        public void RenderScene(Camera camera, Instance[] instances)
+        {
+            Mtx cameraMatrix;
+            Mtx transform;
+            Model clipped;
+
+            // if we want to use FOV here we also need to add the FOV matrix of the camera
+            cameraMatrix = (camera.orientation.Transposed()) * Mtx.MakeTranslationMatrix(-camera.position);
+            for (int i = 0; i < instances.Length; i++)
+            {
+                transform   = (cameraMatrix * instances[i].transform);
+                clipped     = TransformAndClip(camera.clipping_planes.ToArray(), instances[i].model, instances[i].scale, transform);
+
+                if (clipped != null)
+                {
+                    RenderModel(clipped);
+                }
+            }
+
+        }
+        public void FastClear()
+        {
+            unsafe
+            {
+                BitmapData bitmapData = canvas.LockBits(new Rectangle(0, 0, canvas.Width, canvas.Height), ImageLockMode.ReadWrite, canvas.PixelFormat);
+                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(canvas.PixelFormat) / 8;
+                int heightInPixels = bitmapData.Height;
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+                byte* PtrFirstPixel = (byte*)bitmapData.Scan0;
+
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    byte* bits = PtrFirstPixel + (y * bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        bits[x + 0] = 0;// (byte)oldBlue;
+                        bits[x + 1] = 0;// (byte)oldGreen;
+                        bits[x + 2] = 0;// (byte)oldRed;
+                        bits[x + 3] = 0;// (byte)oldRed;
+                    }
+                });
+                canvas.UnlockBits(bitmapData);
+            }
+        }
+        //make a method that scales the instances for a scale s
+        public void Scales(float scale)
+        {
+            for (int i =0; i<instances.Count; i++)
+            {
+                instances[i].transform *= Mtx.MakeScalingMatrix(scale);
+            }
+        }
+        public void Rotate(Vertex angle)
+        {
+            for (int i =0; i < instances.Count; i++)
+            {
+                instances[i].transform *= Mtx.Rotate(angle);
+            }
+        }
+        public void Translate(Vertex translation)
+        {
+            for (int i=0; i<instances.Count; i++)
+            {
+                instances[i].transform *= Mtx.MakeTranslationMatrix(translation);
+            }
+        }
+    }
+}
